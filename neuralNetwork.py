108e8f685a130e017eb1a2e9ab03b611f036a3fa
Praca <jeremipamula@gmail.com>
2024-09-06 11:27:25 +0200

    output changes

--- neuralNetwork.py
+++ neuralNetwork.py
@@ -19,7 +19,10 @@ class neuralNetwork:
         self.lr = learningRate
         self.wih = numpy.random.rand(self.hNodes, self.iNodes) - 0.5
         self.who = numpy.random.rand(self.oNodes, self.hNodes) - 0.5
+        # self.wih = numpy.random.rand( self.iNodes,self.hNodes).T - 0.5
+        # self.who = numpy.random.rand( self.hNodes,self.oNodes).T - 0.5
         self.activation_function = lambda x: scipy.special.expit(x)
+        pass
 
     def query(self, inputs_list):
         inputs = numpy.array(inputs_list, ndmin=2).T
@@ -29,6 +32,7 @@ class neuralNetwork:
         final_inputs = numpy.dot(self.who, hidden_outputs)
         final_outputs = self.activation_function(final_inputs)
         return final_outputs
+        pass
 
     def train(self, inputs_list, targets_list):
         inputs = numpy.array(inputs_list, ndmin=2).T
@@ -43,13 +47,14 @@ class neuralNetwork:
 
         self.who += self.lr * numpy.dot(output_errors * final_outputs * (1.0 - final_outputs), numpy.transpose(hidden_outputs))
         self.wih += self.lr * numpy.dot(hidden_errors * hidden_outputs * (1.0-hidden_outputs),numpy.transpose(inputs))
+        pass
 
 input_nodes = 784
 hidden_nodes = 100
 output_nodes = 10
 
 n = neuralNetwork(input_nodes, hidden_nodes,output_nodes, 0.3)
-all_values = data_list[7].split(',')
+all_values = data_list[2].split(',')
 inputs = (numpy.asarray(all_values[1:],dtype=numpy.float64) / 255.0 * 0.99) +0.01
 image_array = numpy.asarray(all_values[1:],dtype=numpy.float64).reshape((28,28))
 targets = numpy.zeros(output_nodes) + 0.01
@@ -57,7 +62,7 @@ targets = numpy.zeros(output_nodes) + 0.01
 targets[int(all_values[0])] = 0.99
 n.train(inputs, targets)
 
-print(n.query((numpy.asarray(all_values[1:],dtype=numpy.float64)/ 255.0 * 0.99)+0.01))
+print(n.query((numpy.asarray(all_values[1:],dtype=numpy.float64)/ 255.0 * 0.99)+0.01).argmax())
 
 matplotlib.pyplot.imshow(image_array, cmap='Greys',interpolation='None')
 matplotlib.pyplot.show()
